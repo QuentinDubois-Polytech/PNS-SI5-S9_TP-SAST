3ed985775c6d6ea52054b8ac0f861fdc74100844
Supervinh <narutovinh@hotmail.fr>
2024-02-10 10:12:29 -0500

    Xss exploit

--- ex_4/test.c
+++ ex_4/test.c
@@ -15,7 +15,7 @@ int bof(char *str)
 {
 char buffer[BUF_SIZE];
 /* The following statement has a buffer overflow problem */
-strcpy(buffer, str); ¿
+strcpy(buffer, str);
 return 1;
 }
 int main(int argc, char **argv)
@@ -24,7 +24,7 @@ char str[517];
 FILE *badfile;
 /* Change the size of the dummy array to randomize the parameters
 for this lab. Need to use the array at least once */
-char dummy[BUF_SIZE]; memset_s(dummy, 0, BUF_SIZE);
+char dummy[BUF_SIZE]; memset(dummy, 0, BUF_SIZE);
 badfile = fopen("badfile", "r");
 fread(str, sizeof(char), 517, badfile);
 bof(str);
